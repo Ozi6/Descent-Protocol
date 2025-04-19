dfcf154ba894e4ac85b241050cc2d75bc715ee7d
Ozi6 <64336613+Ozi6@users.noreply.github.com>
2025-04-19 16:47:22 +0300

    No More FireRegion
    -Was an experimental idea that I though would be interesting, but does not fit in a 2D top down game. Might reuse in a game that is sidescroll.

--- Assets/Scripts/Entities/Player System/InputHandler.cs
+++ Assets/Scripts/Entities/Player System/InputHandler.cs
@@ -8,18 +8,12 @@ public class InputHandler : MovementComponent
     [SerializeField] private float _holdTimeThreshold = 0.3f; // Time in seconds before considering a hold
     [SerializeField] private float _maxSwipeTime = 0.5f; // Max time for a swipe to be considered valid
 
-    [Header("Screen Regions")]
-    [SerializeField] private bool _useScreenRegions = true;
-    [SerializeField][Range(0, 1)] private float _firingRegionHeight = 0.3f; // Bottom % of screen for firing
-
     private Vector2 _startTouchPosition;
     private Vector2 _currentTouchPosition;
     private float _touchStartTime;
     private bool _isTouching;
     private bool _holdRegistered;
     private GestureState _currentGesture = GestureState.None;
-
-    private Vector2 _screenDimensions;
 
     public event Action<Vector2> OnSwipe;
     public event Action<Vector2> OnTap;
@@ -35,11 +29,6 @@ public class InputHandler : MovementComponent
         Swiping
     }
 
-    private void Start()
-    {
-        _screenDimensions = new Vector2(Screen.width, Screen.height);
-    }
-
     private void Update()
     {
         if(Application.isMobilePlatform)
@@ -48,19 +37,12 @@ public class InputHandler : MovementComponent
             HandleMouseInput();
     }
 
-    private bool IsInFiringRegion(Vector2 position)
-    {
-        if(!_useScreenRegions)
-            return true;
-        return position.y < _screenDimensions.y * _firingRegionHeight;
-    }
-
     private void HandleTouchInput()
     {
-        if (Input.touchCount > 0)
+        if(Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            switch (touch.phase)
+            switch(touch.phase)
             {
                 case TouchPhase.Began:
                     StartTouch(touch.position);
@@ -79,11 +61,11 @@ public class InputHandler : MovementComponent
 
     private void HandleMouseInput()
     {
-        if (Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0))
             StartTouch(Input.mousePosition);
-        else if (Input.GetMouseButton(0))
+        else if(Input.GetMouseButton(0))
             ContinueTouch(Input.mousePosition);
-        else if (Input.GetMouseButtonUp(0))
+        else if(Input.GetMouseButtonUp(0))
             EndTouch(Input.mousePosition);
     }
 
@@ -112,23 +94,19 @@ public class InputHandler : MovementComponent
                 if(distance > _swipeThreshold && touchDuration < _maxSwipeTime)
                 {
                     _currentGesture = GestureState.Swiping;
-                    if(!IsInFiringRegion(_startTouchPosition))
-                        OnSwipe?.Invoke(position - _startTouchPosition);
+                    OnSwipe?.Invoke(position - _startTouchPosition);
                 }
                 else if(touchDuration > _holdTimeThreshold && distance < _swipeThreshold)
                 {
                     _currentGesture = GestureState.Holding;
                     _holdRegistered = true;
-                    if(IsInFiringRegion(_startTouchPosition))
-                        OnHoldStart?.Invoke(position);
+                    OnHoldStart?.Invoke(position);
                 }
                 break;
-
             case GestureState.Holding:
-                if(_holdRegistered && IsInFiringRegion(_startTouchPosition))
+                if(_holdRegistered)
                     OnHold?.Invoke(position);
                 break;
-
             case GestureState.Swiping:
 
                 break;
@@ -148,15 +126,12 @@ public class InputHandler : MovementComponent
             case GestureState.Possible:
                 if(touchDuration < _holdTimeThreshold && distance < _swipeThreshold)
                     OnTap?.Invoke(position);
-                else if (distance > _swipeThreshold && touchDuration < _maxSwipeTime)
-                {
-                    if(!IsInFiringRegion(_startTouchPosition))
-                        OnSwipe?.Invoke(position - _startTouchPosition);
-                }
+                else if(distance > _swipeThreshold && touchDuration < _maxSwipeTime)
+                    OnSwipe?.Invoke(position - _startTouchPosition);
                 break;
 
             case GestureState.Holding:
-                if(_holdRegistered && IsInFiringRegion(_startTouchPosition))
+                if(_holdRegistered)
                     OnHoldEnd?.Invoke();
                 break;
 
@@ -164,7 +139,6 @@ public class InputHandler : MovementComponent
 
                 break;
         }
-
         _isTouching = false;
         _holdRegistered = false;
         _currentGesture = GestureState.None;
